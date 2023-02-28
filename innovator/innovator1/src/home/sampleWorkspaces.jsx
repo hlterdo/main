41a0c6a724e68d4dc9d7167cb0d21adc5b317a21
Halit Erdogan <hlterdo@example.com>
2023-02-28 13:55:58 -0500

    new file:   innovator/innovator1/src/home/sampleWorkspaces.jsx 	deleted:    codelabs/react/node_modules/boostrap/README.md 	deleted:    codelabs/react/node_modules/boostrap/package.json 	modified:   innovator/innovator1/package-lock.json 	modified:   innovator/innovator1/package.json 	deleted:    innovator/innovator1/src/App.css 	modified:   innovator/innovator1/src/App.js 	deleted:    innovator/innovator1/src/index.css 	modified:   innovator/innovator1/src/index.js 	deleted:    innovator/innovator1/src/logo.svg

---
+++ innovator/innovator1/src/home/sampleWorkspaces.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeSampleWorkspaces = () => {
+  return (
+    <div>
+      <h1>Sample Workspaces</h1>
+    </div>
+  );
+};
+
+export default HomeSampleWorkspaces;
